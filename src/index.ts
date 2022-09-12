b68a77e69cd2440cc444e98d9b97b51a35c35009
ferdianzh <ferdianzh30@gmail.com>
2022-09-12 07:17:42 +0700

    integrating dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import { json } from 'body-parser'
+import * as dotenv from 'dotenv'
 
 import { productRouter } from './routes/product.routes'
 import { saleRouter } from './routes/sale.routes'
 
+dotenv.config()
 const app = express()
 app.use(json())
 
 app.use('/api/products', productRouter)
 app.use('/api/sales', saleRouter)
 
-mongoose.connect('mongodb+srv://mono_developer:mono_developer@learn.kndkh.mongodb.net/mono_ecommerce?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@learn.kndkh.mongodb.net/${process.env.MONGO_ATLAS_COLLECTION}?retryWrites=true&w=majority`)
   .then(() => console.log('db connection success'))
   .catch((err) => console.log(`db connection fail : ${err.message}`))
 
-app.listen(3000, () => {
-  console.log('server is listening on port 3000')
+app.listen(process.env.APP_PORT, () => {
+  console.log(`server is listening on port ${process.env.APP_PORT}`)
 })
